5dea54d57643c9dcd5a10e6592b5066d4f24c6ae
Gabriel Bellawere <bellawere@hotmail.com>
2021-05-06 17:38:30 -0300

    adding interface for sheets api

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.recycleview import RecycleView, RecycleViewBehavior
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.properties import BooleanProperty, StringProperty
+from apis.sheetsapi import Sheets as db
 
 kivy.require('2.0.0')
 
@@ -207,13 +208,8 @@ class Main(App):
     #Botões do menu principal
 
     def PressEquips(self, instance):
-        items = [
-            {'l_name':"Vuze", 'l_id':"SW1", 'l_qtd_tot':"1", 'l_qtd_uso':"1", 'l_qtd_def':"0", 'l_qtd_disp':"0", 'l_obs':"Nada a declarar"},
-            {'l_name':"Camera", 'l_id':"SW2", 'l_qtd_tot':"2", 'l_qtd_uso':"1", 'l_qtd_def':"0", 'l_qtd_disp':"1", 'l_obs':"4K"},
-            {'l_name':"Projetor", 'l_id':"SW3", 'l_qtd_tot':"3", 'l_qtd_uso':"1", 'l_qtd_def':"0", 'l_qtd_disp':"2", 'l_obs':"Epson"},
-            {'l_name':"Cabo", 'l_id':"SW4", 'l_qtd_tot':"10", 'l_qtd_uso':"5", 'l_qtd_def':"3", 'l_qtd_disp':"2", 'l_obs':"HDMI"},
-            {'l_name':"Caixa", 'l_id':"SW5", 'l_qtd_tot':"4", 'l_qtd_uso':"2", 'l_qtd_def':"1", 'l_qtd_disp':"1", 'l_obs':"Turbo"}, 
-            ]
+        items = db.GetEquips()
+
         #Adquire o widget atualizado pela função
         eq = self.PopulateEquip(items)
         self.wrapper2.items = items
@@ -225,12 +221,8 @@ class Main(App):
         self.mng.current = self.wrapper2.name
 
     def PressEvents(self, instance):
-        items = [
-            {'l_name':"1", 'l_start':"30/04/2021", 'l_end':"31/04/2021", 'l_team':"Rena", 'l_check':"None", 'l_obs': "Turbo"}, 
-            {'l_name':"2", 'l_start':"30/04/2021", 'l_end':"31/04/2021", 'l_team':"Bada", 'l_check':"Other", 'l_obs': "Blaster"}, 
-            {'l_name':"3", 'l_start':"30/04/2021", 'l_end':"31/04/2021", 'l_team':"Rena", 'l_check':"KTC", 'l_obs': "Master"}, 
-            {'l_name':"Bada", 'l_start':"30/13/2022", 'l_end':"31/82/2022", 'l_team':"Gabriel", 'l_check':"None", 'l_obs': "Leviousa"}
-            ]        
+        items = db.GetEvents()
+
         #Adquire o widget atualizado pela função
         event = self.PopulateEvents(items)
 
@@ -241,12 +233,7 @@ class Main(App):
         self.mng.current = self.wrapper3.name
 
     def PressChecks(self, instance):
-        items = [
-            {'l_name':"1", 'l_items':"2"}, 
-            {'l_name':"2", 'l_items':"3"}, 
-            {'l_name':"3", 'l_items':"4"}, 
-            {'l_name':"Bada", 'l_items':"5"}
-            ]
+        items = db.GetChecks()
         
         #Adquire o widget atualizado pela função
         check = self.PopulateCheck(items)
